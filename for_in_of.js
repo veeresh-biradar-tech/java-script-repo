1e0365ee3ebd392e8870741290b5010d8fe0e5cc
veeresh-biradar-tech <biradarveeresh193@gmail.com>
2025-05-19 11:24:27 +0530

    performed for in, of loop

---
+++ for_in_of.js
@@ -0,0 +1,11 @@
+let nums= [10, 20, 30, 40, 50]
+
+//for in were used to access the index of the array
+for(let idx in nums){
+    console.log(idx)
+}
+
+//for of were used to access the values of the array
+for(let val of nums){
+    console.log(val)
+}
